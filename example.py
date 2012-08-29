7b53b61aeb6928b0dc540007fcf5df66c49321a5
Nullspeaker <nhammond129@gmail.com>
2012-08-29 06:57:58 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,25 @@
-# Note to self: write this example...
+import ch
+class TestBot(ch.RoomManager):
+  def onConnect(self, room):
+    print("Connected")
+    
+  def onReconnect(self, room):
+    print("Reconnected")
+    
+  def onDisconnect(self, room):
+    print("Disconnected")
+  
+  def onMessage(self, room, user, message):
+    print(user.name+":"+message.body)
+    if message.body.startswith("!a"): # Ugh, I should write something proper. Don't do this, folks!
+      room.message("AAAAAAAAAAAAAA")
+
+  
+  def onFloodWarning(self, room):
+    room.reconnect()
+  
+  def onPMMessage(self, pm, user, body):
+    pm.message(user, body) # echo
+
+if __name__ == "__main__":
+  TestBot.easy_start()
