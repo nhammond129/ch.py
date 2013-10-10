5bdc767a78c7e94ef44453290db44a01b16631e5
pystub <pystub@gmail.com>
2013-10-10 14:55:40 +0300

    Update example.py
    that `self` can probably be avoided

--- example.py
+++ example.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 import ch
-import sys
-import codecs
 
 class TestBot(ch.RoomManager):
+  def safePrint (self, text):
+      while True:
+        try:
+          print(text)
+          break
+        except UnicodeEncodeError as ex:
+          text = (text[0:ex.start] + '(unicode)' + text[ex.end:])
+
   def onConnect(self, room):
     room.unicodeCompat = True
     print("Connected to "+room.name)
@@ -16,34 +22,16 @@ class TestBot(ch.RoomManager):
   
   def onMessage(self, room, user, message):
     # Use with PsyfrBot framework? :3
-    def Print(user,message):
-      text = message.body
-      while True:
-        try:
-          print(user.name + ': '+text)
-          break
-        except UnicodeEncodeError as ex:
-          text = (text[0:ex.start] + '(unicode)' + text[ex.end:])
-      
-    Print(user,message)
+    self.safePrint(user.name + ': ' + message.body)
 
     if message.body.startswith("!a"):
       room.message("AAAAAAAAAAAAAA")
 
-  
   def onFloodBan(self, room):
     print("You are flood banned in "+room.name)
   
   def onPMMessage(self, pm, user, body):
-    def Print(user, text):
-      while True:
-        try:
-          print("PM:"+user.name + ': '+text)
-          break
-        except UnicodeEncodeError as ex:
-          text = (text[0:ex.start] + '(unicode)' + text[ex.end:])
-      
-    Print(user, body)
+    self.safePrint('PM: ' + user.name + ': ' + body)
     pm.message(user, body) # echo
 
 if __name__ == "__main__":
